62f563e3466fd080e7713946054e7b7c939cf109
dhanesh-kh <khemrajdhanesh@gmail.com>
2024-10-09 23:42:19 -0400

    match test url with deployment url

--- tests/hexo_test.spec.js
+++ tests/hexo_test.spec.js
@@ -4,6 +4,6 @@ const exp = require('constants')
 
 test('check if hexo exists', async ({page}) => {
 
-    await page.goto('http://localhost:4000/')
+    await page.goto('http://localhost:4000/hexo/')
     await expect(page).toHaveTitle('Index')
 })
